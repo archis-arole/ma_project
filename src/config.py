1f21e0207a8cbae25d6290ca18260485c9bc32fe
archis <archis.arole@gmail.com>
2026-04-07 11:43:15 +0530

    I have captialized the variable params. Now they will be used elsewhere.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import datetime
 
-change_date = datetime.date(2024, 7, 5)
-start_date = datetime.date(2016, 4, 6)
-end_date = datetime.date(2026, 4, 6)
+CHANGE_DATE = datetime.date(2024, 7, 5)
+START_DATE = datetime.date(2016, 4, 6)
+END_DATE = datetime.date(2026, 4, 6)
