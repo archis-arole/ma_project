b87b24a9f8f9e05fc85c1bf54aebc88f79d7be2d
archis <archis.arole@gmail.com>
2026-04-08 10:57:58 +0530

    Moved plot equity curve function to utils.py

--- src/model.py
+++ src/model.py
@@ -6,17 +6,6 @@ import seaborn as sns
 sns.set_theme()
 
 
-def plot_equity_curve(equity_curve):
-    """Plot the equity curve over time."""
-    plt.figure(figsize=(10, 5))
-    plt.plot(equity_curve.index, equity_curve.values)
-    plt.xlabel("Date")
-    plt.ylabel("Equity")
-    plt.title("Equity Curve")
-    plt.tight_layout()
-    plt.show()
-
-
 def model(params, prices):
     '''
     This is the moving average model.
