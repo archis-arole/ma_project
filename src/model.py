812959ce8b230a24b41b964e8a1f9f1efd3551bc
archis <archis.arole@gmail.com>
2026-04-09 14:28:36 +0530

    Have prototyped slippage logic.

--- src/model.py
+++ src/model.py
@@ -41,26 +41,34 @@ def model(params, front_df, next_df):
     This is the moving average model.
     sp is short period of moving average.
     lp is long period of moving average.
-    Also expects long/short ratio.
-    Note that this function is vectorized.
+    For now this model does not use leverage.
+    So we remain entirely invested only with the equity we have.
+
+    Inputs:
+    params: It is an array of 2 numbers: sp and lp in that order
+    front_df: It is the DataFrame of the front month futures
+    next_df: It is the DataFrame of the next month futures
     '''
     combined_df, roll_indices = rollover(front_df, next_df)
     prices = combined_df['PRICE']
     returns = prices.pct_change()
-    sp, lp, long_short_ratio = params
-    # For now I don't want to introduce leverage
-    long_short_ratio = 1
+    sp, lp = params
+
     ma_short = prices.rolling(sp).mean().shift(1)[lp + 1:]
     ma_long = prices.rolling(lp).mean().shift(1)[lp + 1:]
     signal = ma_long < ma_short
     trade = signal.ne(signal.shift(1))
     trade.iloc[0] = True
+
     returns_aligned = returns.reindex(signal.index).dropna()
     signal_aligned = signal.loc[returns_aligned.index]
     model_returns = returns_aligned * np.where(
-        signal_aligned, long_short_ratio, -1
+        signal_aligned, 1, -1
     )
-    equity_curve = (1 + model_returns).cumprod()
+
+    cost = pd.Series(0.0, index=model_returns.index)
+    slippage_bps = 0.0002
+    cost += slippage_bps * int(trade)
     return equity_curve, model_returns
 
 
